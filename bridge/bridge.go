91d4ee40ade2beb7e42831c4e97a591f169b6051
cocofhu <cocofhu@outlook.com>
2025-05-02 00:42:05 +0800

    修复registry镜像通过ENV指定端口 存活检查失败的问题

--- bridge/bridge.go
+++ bridge/bridge.go
@@ -357,11 +357,10 @@ func (b *Bridge) newService(port ServicePort, isgroup bool) *Service {
 	delete(metadata, "id")
 	delete(metadata, "tags")
 	delete(metadata, "name")
-
+	// fix：端口在ENV里改变时，进行存活检查时未即使同步
+    // 从metadata里覆盖端口
 	if p, ok := metadata["port"]; ok {
-        if x, e := strconv.Atoi(p); e == nil {
-            // fix：端口在ENV里改变时，进行存活检查时未即使同步
-            // 从metadata里覆盖端口
+		if x, e := strconv.Atoi(p); e == nil {
             service.Port = x
         }
 	}
